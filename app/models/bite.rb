235530b5478b24892af47a778f1acb0f95683b88
David Chawei Hsu <chawei.hsu@gmail.com>
2010-12-11 23:24:31 -0800

    fix related image bug ...

--- app/models/bite.rb
+++ app/models/bite.rb
@@ -8,7 +8,7 @@ class Bite < ActiveRecord::Base
       uri = URI(self.url)
       return "http://#{uri.host}#{new_image_url}"
     else 
-      if match = self.image_url.match(/^\w.*/)
+      if match = self.image_url.match(/^[\.\w].*/)
         new_image_url = match[0]
       end
       uri = self.url.match(/(.*\/)/)[0]
