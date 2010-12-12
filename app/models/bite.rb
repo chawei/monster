603d0e953dc4f0d3d59a0705b59332adf4d90b3a
David Chawei Hsu <chawei.hsu@gmail.com>
2010-12-11 23:14:22 -0800

    fix related image path again ...

--- app/models/bite.rb
+++ app/models/bite.rb
@@ -3,12 +3,16 @@ class Bite < ActiveRecord::Base
   def normalized_image_url
     if self.image_url =~ /^http/
       return self.image_url
-    else
-      if match = self.image_url.match(/(\/.*)/)
-        new_image_url = match[0]
-      end
+    elsif match = self.image_url.match(/^\/.*/)
+      new_image_url = match[0]
       uri = URI(self.url)
       return "http://#{uri.host}#{new_image_url}"
+    else 
+      if match = self.image_url.match(/^\w.*/)
+        new_image_url = match[0]
+      end
+      uri = self.url.match(/(.*\/)/)[0]
+      return "http://#{uri}#{new_image_url}"
     end
   end
   
