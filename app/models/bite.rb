ef1a53062b62cd51002a8041d29d297097228d96
yuin <stopcloud@gmail.com>
2011-01-24 07:21:38 -0800

    display 500 imgs instead of 200

--- app/models/bite.rb
+++ app/models/bite.rb
@@ -25,6 +25,6 @@ class Bite < ActiveRecord::Base
   
     def set_accessible
       res = HTTParty.get(URI.escape(self.normalized_image_url))
-      res.code == 200 ? self.accessible = true : self.accessible = false
+      res.code == 500 ? self.accessible = true : self.accessible = false
     end
 end
