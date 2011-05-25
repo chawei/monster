7856950612dc63232327776b17eb590aff12ba14
David Chawei Hsu <chawei.hsu@gmail.com>
2011-05-25 09:25:00 -0700

    fix bite count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   
   def date
     @date = params[:date].to_date
-    @bites = Bite.on(@date)
+    @bites = Bite.public_facing.on(@date).order('bites.created_at DESC')
     @top_sources = Bite.top_sources_on(@date).limit(10).map { |b| [b.domain_name, b.cnt] }
   end
   
